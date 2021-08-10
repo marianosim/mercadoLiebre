cedebf9c60141e3744c803da3a46d76af2b2383f
marianosim <mariano.simantob@gmail.com>
2021-08-10 19:09:52 -0300

    modified app.listen port

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ const path = require('path');
 const publicPath = path.resolve(__dirname, './public');
 app.use(express.static(publicPath));
 
-app.listen(3030, () => {
-    console.log('Servidor corriendo en puerto 3030');
-});
+app.listen(process.env.PORT || 3000, function() {
+	console.log(`Servidor corriendo en el puerto 3000`);
+})
 
 app.get('/', (req, res)=> {
     res.sendFile(path.resolve(__dirname, './views/home.html'))
